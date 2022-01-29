17739294fd1ba71f27de9417d83651442330199b
tomquinn04 <72301139+tomquinn04@users.noreply.github.com>
2022-01-29 21:03:01 +0000

    move to dbmodule

--- Project1.dpr
+++ Project1.dpr
@@ -6,14 +6,14 @@ uses
   dashboard in 'dashboard.pas' {frameDashboard: TFrame},
   transactions in 'transactions.pas' {frameTransactions: TFrame},
   artists in 'artists.pas' {frameArtists: TFrame},
-  dbconnection in 'dbconnection.pas' {fDbConnection};
+  datamodule in 'datamodule.pas' {DataModule1: TDataModule};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TDataModule1, DataModule1);
   Application.CreateForm(TForm1, Form1);
-  Application.CreateForm(TfDbConnection, fDbConnection);
   Application.Run;
 end.
