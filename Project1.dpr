9bbddc3342862181f04173946370fe9bcacbe14d
tomquinn04 <72301139+tomquinn04@users.noreply.github.com>
2022-02-20 21:43:01 +0000

    SplashScreen

--- Project1.dpr
+++ Project1.dpr
@@ -8,14 +8,30 @@ uses
   dashboard in 'dashboard.pas' {frameDashboard: TFrame},
   transactions in 'transactions.pas' {frameTransactions: TFrame},
   artists in 'artists.pas' {frameArtists: TFrame},
-  datamodule in 'datamodule.pas' {DataModule1: TDataModule};
+  datamodule in 'datamodule.pas' {DataModule1: TDataModule},
+  splash in 'splash.pas' {frmSplash},
+  System.SysUtils;
+
 
 {$R *.res}
+
+var
+  SplashScreen: Tfrmsplash;
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  // -- Splash Screen -- //
+  SplashScreen := Tfrmsplash.Create(nil);
+  SplashScreen.Show;
+  SplashScreen.Update;
+  Sleep(3000);
+  // ------------------- //
+
   Application.CreateForm(TDataModule1, DataModule1);
   Application.CreateForm(TForm1, Form1);
+  SplashScreen.Hide;
+  SplashScreen.Free;
+
   Application.Run;
 end.
